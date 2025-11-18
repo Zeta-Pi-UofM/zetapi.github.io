d95cd75d4d81a41a7e9f03a0ed9631f5ba2fad7a
nicknastoski <nnastoski1@gmail.com>
2025-11-18 01:15:21 -0500

    Update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,21 @@
-import js from '@eslint/js'
+import eslint from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import { defineConfig } from 'eslint/config'
 
-export default tseslint.config([
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite,
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
+      ecmaVersion: 2025,
+      globals: globals.browser
+    }
+  }
+)
